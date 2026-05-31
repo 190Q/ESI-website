6889db074ffdd44ba91bbcf3da8f5414f4cd43c8
190Q <im190Q@gmail.com>
2026-05-31 12:50:34 +0200

    added themes (well just the default one for now)

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 
 // Import all CSS (Vite bundles these into a single file)
+import '../../css/themes.css'
 import '../../css/base.css'
 import '../../css/popup.css'
 import '../../css/graph-shared.css'
@@ -16,5 +17,14 @@ import '../../css/shop.css'
 import '../../css/shop-admin.css'
 import '../../css/creator-studio.css'
 import '../../css/auth-gate.css'
+
+// Theme switcher (call setTheme('name') in the DevTools console)
+const savedTheme = localStorage.getItem('theme');
+if (savedTheme) document.documentElement.setAttribute('data-theme', savedTheme);
+
+window.setTheme = (name) => {
+  document.documentElement.setAttribute('data-theme', name);
+  localStorage.setItem('theme', name);
+};
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />)
